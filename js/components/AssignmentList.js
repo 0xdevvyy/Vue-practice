c20f1762ddfb81c2582d6c5f43656e25ed6900c5
0xdevvyy <pullhead45@gmail.com>
2025-12-06 12:48:15 +0800

    feat: playing around

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -3,10 +3,24 @@ export default {
     components:{Assignment},
     template: `
          <section v-show="assignments.length">
-            <h2 class="font-bold text-blue mb-2">{{title}}</h2>
-            <ul class="border border-white divide-y">
+            <h2 class="font-bold text-blue mb-2">{{title}}
+                <span>({{assignments.length}})</span>
+            </h2>
+            <div class="flex gap-2">
+                <button 
+                v-for="tag in tags" 
+                class="border rounded px-1 py-px text-sm"
+                :class = "{
+                    'border border-blue-600' : tag === currentTag
+                }"
+                @click="currentTag = tag"
+                >
+                    {{tag}}
+                </button>
+            </div>
+            <ul class="border border-white divide-y mt-4">
                <assignment
-                v-for="assignment in assignments" 
+                v-for="assignment in filteredAssignment" 
                 :key="assignment.id"
                 :assignment="assignment"></assignment>
             </ul>
@@ -15,5 +29,20 @@ export default {
     props: {
         assignments: Array,
         title: String
+    },
+    data(){
+        return {currentTag: 'all'}
+    },
+    computed: {
+        filteredAssignment(){
+            if(this.currentTag === 'all'){
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag === this.currentTag)
+        },
+        tags(){
+            return ['all' , ...new Set(this.assignments.map(a => a.tag))]
+        }
+        
     }
 }
